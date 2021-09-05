c40e1c50ddab5c6ae7686cb8ac13daf8b7fbde75
cyberpwn <danielmillst@gmail.com>
2021-09-05 19:54:45 -0400

    Fix setup

---
+++ src/main/java/com/volmit/phantom/plugin/PhantomPlugin.java
@@ -0,0 +1,15 @@
+package com.volmit.phantom.plugin;
+
+import art.arcane.amulet.geometry.Vec3;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PhantomPlugin extends JavaPlugin {
+    public void test()
+    {
+
+    }
+}
